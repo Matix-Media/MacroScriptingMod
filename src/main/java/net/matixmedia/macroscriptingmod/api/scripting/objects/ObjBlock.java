d8206ef753ee85b8a2f464b0d3823c1398fa2ba7
Matix-Media <support@matix-media.net>
2022-12-05 19:53:13 +0100

    Changed block id type

--- src/main/java/net/matixmedia/macroscriptingmod/api/scripting/objects/ObjBlock.java
+++ src/main/java/net/matixmedia/macroscriptingmod/api/scripting/objects/ObjBlock.java
@@ -4,6 +4,7 @@ import net.matixmedia.macroscriptingmod.api.scripting.Obj;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 
 public class ObjBlock extends Obj {
 
@@ -11,7 +12,7 @@ public class ObjBlock extends Obj {
     public int x, y, z;
 
     public ObjBlock(BlockPos pos, Block block) {
-        this.name = block.getTranslationKey();
+        this.name = Registry.BLOCK.getId(block).toString();
         this.x = pos.getX();
         this.y = pos.getY();
         this.z = pos.getZ();
