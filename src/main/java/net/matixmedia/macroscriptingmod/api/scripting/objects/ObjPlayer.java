dcd0f77854c63d328d1951c45b88f5706e7e5fe5
Matix-Media <support@matix-media.net>
2022-12-16 18:47:54 +0100

    Added player.display_name to ObjPlayer

--- src/main/java/net/matixmedia/macroscriptingmod/api/scripting/objects/ObjPlayer.java
+++ src/main/java/net/matixmedia/macroscriptingmod/api/scripting/objects/ObjPlayer.java
@@ -1,21 +1,28 @@
 package net.matixmedia.macroscriptingmod.api.scripting.objects;
 
 import net.matixmedia.macroscriptingmod.api.scripting.Obj;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
 
 public class ObjPlayer extends Obj {
 
-    private String name;
-    private String UUID;
+    public final String name;
+    public final String display_name;
+    public final String UUID;
 
     public ObjPlayer(ClientPlayerEntity player) {
         this.name = player.getGameProfile().getName();
         this.UUID = player.getUuid().toString();
+        this.display_name = player.getDisplayName().getString();
     }
 
     public ObjPlayer(PlayerListEntry playerListEntry) {
+        MinecraftClient mc = MinecraftClient.getInstance();
+        String displayName = mc.inGameHud.getPlayerListHud().getPlayerName(playerListEntry).getString();
+
         this.name = playerListEntry.getProfile().getName();
         this.UUID = playerListEntry.getProfile().getId().toString();
+        this.display_name = displayName;
     }
 }
--- src/main/java/net/matixmedia/macroscriptingmod/scripting/libs/LibServer.java
+++ src/main/java/net/matixmedia/macroscriptingmod/scripting/libs/LibServer.java
@@ -3,7 +3,6 @@ package net.matixmedia.macroscriptingmod.scripting.libs;
 import net.matixmedia.macroscriptingmod.MacroScriptingMod;
 import net.matixmedia.macroscriptingmod.api.scripting.AutoLibFunction;
 import net.matixmedia.macroscriptingmod.api.scripting.Lib;
-import net.matixmedia.macroscriptingmod.api.scripting.LibArgFunction;
 import net.matixmedia.macroscriptingmod.api.scripting.LibZeroArgFunction;
 import net.matixmedia.macroscriptingmod.api.scripting.objects.ObjPlayer;
 import net.minecraft.client.MinecraftClient;
@@ -13,20 +12,12 @@ import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
-import net.minecraft.network.Packet;
-import net.minecraft.network.listener.PacketListener;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.luaj.vm2.LuaValue;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LibServer extends Lib {
-    private static Map<String, Class<? extends Packet<? extends PacketListener>>> PACKET_MAPPINGS = new HashMap<>() {{
-       put("move_packet", PlayerMoveC2SPacket.Full.class);
-    }};
 
     public LibServer() {
         super("server");
@@ -49,12 +40,12 @@ public class LibServer extends Lib {
     public static class GetOnlinePlayers extends LibZeroArgFunction {
         @Override
         public LuaValue call() {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            if (mc.player == null) return NIL;
+            if (this.getMinecraft().player == null) return NIL;
 
             List<LuaValue> entries = new ArrayList<>();
-            for (PlayerListEntry entry : mc.player.networkHandler.getPlayerList())
+            for (PlayerListEntry entry : this.getMinecraft().player.networkHandler.getPlayerList()) {
                 entries.add(new ObjPlayer(entry).toLua());
+            }
 
             return LuaValue.listOf(entries.toArray(new LuaValue[0]));
         }
